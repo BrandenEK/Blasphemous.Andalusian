809de94cb64345d3252c22cc2406ba6689611659
unknown <brandenek03912@hotmail.com>
2024-02-16 11:47:23 -0500

    core: replace old files with new api code

---
+++ Blasphemous.Andalusian/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace Blasphemous.Andalusian;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("Blasphemous.ModdingAPI", "2.1.0")]
+internal class Main : BaseUnityPlugin
+{
+    public static Andalusian Andalusian { get; private set; }
+
+    private void Start()
+    {
+        Andalusian = new Andalusian();
+    }
+}
